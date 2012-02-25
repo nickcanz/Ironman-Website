e96fd271462785fd768f4f0c55bc1e7bc7de9ce4
ncanzone <nicholas.canzoneri@algorithmics.com>
2012-02-24 20:25:59 -0500

    Scaffolding out the rest of the website

---
+++ Website/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //
+        // GET: /Projects/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Website/Controllers/RatesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class RatesController : Controller
+    {
+        //
+        // GET: /Rates/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
